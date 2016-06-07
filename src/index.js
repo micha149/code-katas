e6e299c66c83213b1c60f8e6391710282ceb4e7a
Michael van Engelshoven <michael@van-engelshoven.de>
2016-06-07 10:03:49 +0200

    Add some example code
    This makes it easier for getting in and also validates that the test
    setup is running

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export function example(count) {
+    return count;
+}
---
+++ test/indexTest.js
@@ -0,0 +1,9 @@
+import {example} from '../src';
+
+describe('FizzBuzz', () => {
+
+    it('just works', () => {
+        expect(example(1)).to.be.equal(1);
+    });
+
+});
