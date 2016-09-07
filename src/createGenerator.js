5a10597cbe060198d270f6cb402f3f2819646ddd
Michael van Engelshoven <michael@van-engelshoven.de>
2016-09-07 15:24:44 +0200

    Refactored implementation code

--- src/createGenerator.js
+++ src/createGenerator.js
@@ -1,7 +1,11 @@
-export const createGenerator = (...transformers) => {
-    return (num) => {
-        return transformers.map(transformer => transformer(num))
-            .filter(str => str !== null)
-            .reduce((sum, val, idx) => idx === 0 ? val : sum += val, num);
+export const createGenerator = (...transformers) => num => {
+    const strings = transformers
+        .map(transformer => transformer(num))
+        .filter(str => str !== null);
+
+    if (strings.length === 0) {
+        return num;
     }
+
+    return strings.join('');
 }
