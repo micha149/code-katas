5064a34844076762f1813687aff62c94d2e7c97e
Michael van Engelshoven <michael@van-engelshoven.de>
2016-09-09 05:38:48 +0200

    Implement roman numbers kata

--- src/RomanNumbers.js
+++ src/RomanNumbers.js
@@ -1,3 +1,15 @@
+const MAP = [
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+]
+
 /**
  * Roman Numbers service to convert numbers.
  */
@@ -8,7 +20,16 @@ export class RomanNumbers {
      * @return {string} Roman number
      */
     get(num) {
-        // TODO write your code :)
+        let result = '';
+        let rest = num;
+
+        while (rest > 0) {
+            const next = MAP.find(([decimal]) => decimal <= rest);
+            rest -= next[0];
+            result += next[1];
+        }
+
+        return result;
     }
 
 }
--- test/RomanNumbersTest.js
+++ test/RomanNumbersTest.js
@@ -17,8 +17,29 @@ describe('RomanNumbers', () => {
         romanNumbers = new RomanNumbers();
     });
 
-    it(`converts 1 to I`, () => {
-        expect(romanNumbers.get(1)).toEqual('I');
-    });
+    const testValues = [
+        [1, 'I'],
+        [2, 'II'],
+        [3, 'III'],
+        [4, 'IV'],
+        [5, 'V'],
+        [6, 'VI'],
+        [7, 'VII'],
+        [8, 'VIII'],
+        [9, 'IX'],
+        [10, 'X'],
+        [14, 'XIV'],
+        [16, 'XVI'],
+        [19, 'XIX'],
+        [29, 'XXIX'],
+        [90, 'XC'],
+        [149, 'CXLIX'],
+    ];
+
+    testValues.forEach(([decimal, roman]) => {
+        it(`converts ${decimal} to ${roman}`, () => {
+            expect(romanNumbers.get(decimal)).toEqual(roman);
+        });
+    })
 
 });
